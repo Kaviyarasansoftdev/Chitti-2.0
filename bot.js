c3b2ea5b2115f9cb304a2ba07e0166d6b2177387
Kaviyarasansoftdev <kaviyarasan.tcz@gmail.com>
2024-02-23 00:42:56 +0530

    new

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ const searchInput = document.getElementById('search');
 searchInput.addEventListener('input', function() {
   const query = searchInput.value.trim(); // Trim any leading or trailing spaces
   
-  if (query !== ''&& query.length >= 4) {
+  if (query !== '' && query.length >= 4) {
     console.log(query);
     searchdata(query);
   } else {
-    // Clear the output div when the input is empty
+    // Clear the output div when the input is empty or less than 4 characters
     const outputDiv = document.getElementById('output');
     outputDiv.innerHTML = '';
   }
@@ -37,13 +37,29 @@ function displayData(data) {
   data.forEach(post => {
     const postDiv = document.createElement('div');
     postDiv.classList.add('resultdiv');
+    
     if (post?.images) {
       const img = document.createElement('img');
       img.src = post?.images;
       img.classList.add('resultimg');
       postDiv.appendChild(img);
     }
-    postDiv.innerHTML += `<h2>${post?.searchdata}</h2>`;
+    
+    const postContent = document.createElement('div');
+    postContent.classList.add('post-content');
+    
+    const heading = document.createElement('h2');
+    heading.textContent = post?.searchdata;
+    postContent.appendChild(heading);
+    
+    const link = document.createElement('a');
+    link.href = post?.link;
+    link.textContent = "Read More...";
+    link.classList.add('read-more'); // Adding class 'read-more' to the link
+    link.target = "_blank"; // Open link in a new tab
+    postContent.appendChild(link);
+    
+    postDiv.appendChild(postContent);
     outputDiv.appendChild(postDiv);
   });
 }
