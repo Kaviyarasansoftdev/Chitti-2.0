b19e61a51acb57289bdc985ddaa462bffed105e4
Kaviyarasansoftdev <kaviyarasan.tcz@gmail.com>
2024-02-22 23:35:15 +0530

    new

---
+++ bot.js
@@ -0,0 +1,49 @@
+const searchInput = document.getElementById('search');
+
+searchInput.addEventListener('input', function() {
+  const query = searchInput.value.trim(); // Trim any leading or trailing spaces
+  
+  if (query !== ''&& query.length >= 4) {
+    console.log(query);
+    searchdata(query);
+  } else {
+    // Clear the output div when the input is empty
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '';
+  }
+});
+
+function searchdata(query) {
+  fetch('./dataset/feeddata.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const regex = new RegExp('\\b' + query + '\\b', 'i');
+      const resq = data.filter(item => regex.test(item.searchdata));
+      displayData(resq);
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+function displayData(data) {
+  const outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = '';
+  data.forEach(post => {
+    const postDiv = document.createElement('div');
+    postDiv.classList.add('resultdiv');
+    if (post?.images) {
+      const img = document.createElement('img');
+      img.src = post?.images;
+      img.classList.add('resultimg');
+      postDiv.appendChild(img);
+    }
+    postDiv.innerHTML += `<h2>${post?.searchdata}</h2>`;
+    outputDiv.appendChild(postDiv);
+  });
+}
